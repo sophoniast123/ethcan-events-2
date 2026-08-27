ccf2207b9f9eed2b074eda507eabae039fad0da3
Sophonias Tamene <sophoniastamene123@gmail.com>
2026-08-27 12:30:09 +0300

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
